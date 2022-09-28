36d5d6e7266958d7eeb0ca828d4f50f0b8ae69f1
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-09-28 12:22:59 -0700

    userContext

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,1 +1,15 @@
-// TODO -- create the user provider!
+// TODO -- create the user provider!same as from class 4childrenprop
+import { createContext, useState } from 'react';
+import { getUser } from '../services/auth';
+
+// Call createContext to create UserContext that's passable
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const currentUser = getUser();
+  const [user, setUser] = useState(currentUser);
+
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+};
+
+export { UserContext, UserProvider };
